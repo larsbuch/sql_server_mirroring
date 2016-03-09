42c042935b63a1d846082691c98ac8524b042dfd
Lars Shakya buch-Jepsen <lbj@applied-intelligence.dk>
2016-03-09 12:19:17 +0100

    SMO implementation

---
+++ sql_server_mirroring/SqlServerMirroringTester/Program.cs
@@ -0,0 +1,24 @@
+﻿using ConsoleTester;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlServerMirroringTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleTest.AddTest("SQL Server Connection", "Try to connect to server with SMO", ()=>TestConnectToSMO());
+
+            ConsoleTest.Run();
+        }
+
+        private static void TestConnectToSMO()
+        {
+
+        }
+    }
+}
