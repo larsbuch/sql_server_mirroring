09387de8b482f2aea4772ea5caf01af882e9de12
Lars Shakya buch-Jepsen <lbj@applied-intelligence.dk>
2016-03-09 22:12:50 +0100

    Partial

--- sql_server_mirroring/SqlServerMirroring/MirrorState.cs
+++ sql_server_mirroring/SqlServerMirroring/MirrorState.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.SqlServer.Management.Smo;
 
 namespace SqlServerMirroring
 {
     public class MirrorState
     {
+        private Database database;
+
+        public MirrorState(Database database)
+        {
+            this.database = database;
+        }
+
+        public string DatabaseName { get; internal set; }
+        public bool IsConfiguredForMirroring { get; internal set; }
+        public bool IsGoodMirrorState { get; internal set; }
     }
 }
